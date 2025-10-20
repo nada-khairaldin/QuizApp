a6bca77c9f50482467246907c2985b0f9b9c331d
nada khairaldin <nadakhairaldin8@gmail.com>
2025-10-20 14:36:22 +0300

    add active status and set up question component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { useEffect, useReducer } from "react";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 
 const initialState = {
   questions: [],
@@ -13,27 +17,36 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "dataRecived":
       return { ...state, questions: action.payload, status: "ready" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState); //destructing state into status & questions, easier than state.questions/status
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecived", payload: data }))
-      .catch(dispatch({ type: "dataFaild" }));
+      .catch((err) => dispatch({ type: "dataFaild" }));
   }, []);
 
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />  //passed dispatch as a prop like what we have used to do with setState, to change state on click button  
+        )}
+        {status === "active" && <Question />}
+        {console.log(status)}
       </Main>
     </div>
   );
---
+++ src/components/Question.jsx
@@ -0,0 +1,9 @@
+function Question() {
+    return (
+        <div>
+            qqq
+        </div>
+    )
+}
+
+export default Question
